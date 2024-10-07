2ff31f89faba3c75e31e77b67d6687e71d7fbece
Anfo <longjiahui@hotmail.com>
2024-10-07 20:05:47 +0800

    feat: tsversion- v1.0.0

--- src/index.ts
+++ src/index.ts
@@ -1,105 +1,104 @@
+export type * from "./typings"
 import type {
-    AwaitedRuleReturn,
-    Module,
-    Rule,
-    RuleArg,
-    RuleParser,
-} from './typings'
-import boolean from './modules/boolean'
-import object from './modules/object'
-import string from './modules/string'
-import array from './modules/array'
-import regexp from './modules/regexp'
-import number from './modules/number'
-import presetRules from './presetRules'
+  AwaitedRuleReturn,
+  Module,
+  Rule,
+  RuleArg,
+  RuleParser,
+} from "./typings"
+import boolean from "./modules/boolean"
+import object from "./modules/object"
+import string from "./modules/string"
+import array from "./modules/array"
+import regexp from "./modules/regexp"
+import number from "./modules/number"
+import presetRules from "./presetRules"
 
 export class Validator {
-    rules: Partial<Record<any, RuleArg>> = {}
-    parsers: RuleParser[] = []
+  rules: Partial<Record<any, RuleArg>> = {}
+  parsers: RuleParser[] = []
 
-    constructor(
-        public options: {
-            logger?: Console
-            // level?: keyof Console
-        } = {
-            // level: 'log',
+  constructor(
+    public options: {
+      logger?: Console
+      // level?: keyof Console
+    } = {
+      // level: 'log',
+    }
+  ) {
+    this.use(boolean)
+    this.use(object)
+    this.use(string)
+    this.use(array)
+    this.use(regexp)
+    this.use(number)
+    this.addRules(presetRules)
+  }
+
+  checkAnd(promises: Promise<AwaitedRuleReturn>[]): Promise<AwaitedRuleReturn> {
+    return Promise.all(promises).then((reses) => {
+      return reses.filter((r) => !!r).join(";")
+    })
+  }
+  // a||b <==> !(!a&&!b)
+  checkOr(promises: Promise<AwaitedRuleReturn>[]) {
+    function invert(promise: Promise<AwaitedRuleReturn>) {
+      return promise.then((res) => (res ? "" : "checkOr: !some is true"))
+    }
+    return invert(this.checkAnd((promises || []).map((p) => invert(p))))
+  }
+
+  use(mod: Module) {
+    let parser = mod.getParser(this)
+    if (parser) {
+      this.parsers.push(parser)
+    }
+  }
+
+  addRule(key: any, rule: RuleArg) {
+    this.rules[key] = rule
+  }
+  addRules(rules: Partial<Record<any, Rule>>) {
+    Object.keys(rules).forEach((k) => {
+      if (rules[k]) {
+        this.addRule(k, rules[k]!)
+      }
+    })
+  }
+  getRule(key: any) {
+    return this.rules[key]
+  }
+
+  v(...rest: Parameters<typeof this.validate>) {
+    return this.validate(...rest)
+  }
+
+  async validate(
+    val: any,
+    rule: RuleArg,
+    silent: boolean = false
+  ): Promise<AwaitedRuleReturn> {
+    for (let parser of this.parsers) {
+      let parsedRule = parser.parseRule(rule)
+      if (parsedRule) {
+        const ret = await this.validate(val, parsedRule, true)
+        if (ret && !silent) {
+          this.options.logger?.error(ret)
         }
-    ) {
-        this.use(boolean)
-        this.use(object)
-        this.use(string)
-        this.use(array)
-        this.use(regexp)
-        this.use(number)
-        this.addRules(presetRules)
+        return ret
+      }
     }
+    // 每条规则都会到这里验证
+    if (rule && typeof rule === "function") {
+      const ret = await rule.call(this, val, this)
+      if (ret && !silent) {
+        this.options.logger?.error(ret)
+      }
+      return ret
+    }
+  }
 
-    checkAnd(
-        promises: Promise<AwaitedRuleReturn>[]
-    ): Promise<AwaitedRuleReturn> {
-        return Promise.all(promises).then((reses) => {
-            return reses.filter((r) => !!r).join(';')
-        })
-    }
-    // a||b <==> !(!a&&!b)
-    checkOr(promises: Promise<AwaitedRuleReturn>[]) {
-        function invert(promise: Promise<AwaitedRuleReturn>) {
-            return promise.then((res) => (res ? '' : 'checkOr: !some is true'))
-        }
-        return invert(this.checkAnd((promises || []).map((p) => invert(p))))
-    }
-
-    use(mod: Module) {
-        let parser = mod.getParser(this)
-        if (parser) {
-            this.parsers.push(parser)
-        }
-    }
-
-    addRule(key: any, rule: RuleArg) {
-        this.rules[key] = rule
-    }
-    addRules(rules: Partial<Record<any, Rule>>) {
-        Object.keys(rules).forEach((k) => {
-            if (rules[k]) {
-                this.addRule(k, rules[k]!)
-            }
-        })
-    }
-    getRule(key: any) {
-        return this.rules[key]
-    }
-
-    v(...rest: Parameters<typeof this.validate>) {
-        return this.validate(...rest)
-    }
-
-    async validate(
-        val: any,
-        rule: RuleArg,
-        silent: boolean = false
-    ): Promise<AwaitedRuleReturn> {
-        for (let parser of this.parsers) {
-            let parsedRule = parser.parseRule(rule)
-            if (parsedRule) {
-                const ret = await this.validate(val, parsedRule, true)
-                if (ret && !silent) {
-                    this.options.logger?.error(ret)
-                }
-                return ret
-            }
-        }
-        // 每条规则都会到这里验证
-        if (rule && typeof rule === 'function') {
-            const ret = await rule.call(this, val, this)
-            if (ret && !silent) {
-                this.options.logger?.error(ret)
-            }
-            return ret
-        }
-    }
-
-    joinError(...rest: (string | undefined | null)[]) {
-        return rest.filter((d) => !!d).join('\n')
-    }
+  joinError(...rest: (string | undefined | null)[]) {
+    return rest.filter((d) => !!d).join("\n")
+  }
 }
--- src/typings/index.ts
+++ src/typings/index.ts
@@ -1,41 +1,49 @@
-import type { Validator } from '..'
+import type { Validator } from ".."
+import type { default as presetRules } from "../presetRules"
 
 export type Rule<D = any> = (
-    val: D,
-    v: Validator,
-    errorDescription?: string
+  val: D,
+  v: Validator,
+  errorDescription?: string
 ) => PromiseLike<string | void | undefined> | string | void | undefined
 export type RuleReturn = ReturnType<Rule>
 export type AwaitedRuleReturn = Awaited<RuleReturn>
 export interface Module {
-    getParser(v: Validator): RuleParser
+  getParser(v: Validator): RuleParser
 }
 
+export type PresetRules = keyof typeof presetRules
+
 export interface RuleExtend {
-    base: Rule
-    string: string
-    boolean: boolean
-    regexp: RegExp
-    number: number
+  base: Rule
+  string: string
+  boolean: boolean
+  regexp: RegExp
+  number: number
 }
 
 export type GetRuleArg<T> = T[keyof T]
 export type RuleArg =
-    | GetRuleArg<RuleExtend>
-    | {
-          /** for object recursive*/
-          [k: string]: RuleArg | undefined
-          $or?: RuleArg[]
-          $subItem?: RuleArg
-      }
-    | /*for and */ RuleArg[]
-    | {
-          $or?: RuleArg[]
-          $subItem?: RuleArg
-      }
+  | PresetRules
+  | GetRuleArg<RuleExtend>
+  | {
+      /** for object recursive*/
+      [k: string]: RuleArg | undefined
+      $or?: RuleArg[]
+      $subItem?: RuleArg
+    }
+  | /*for and */ RuleArg[]
+  | {
+      $or?: RuleArg[]
+      $subItem?: RuleArg
+    }
+
+export type ObjectRules<T extends object> = Partial<
+  Record<keyof T extends string ? `${keyof T}$` | keyof T : never, RuleArg>
+>
 
 export interface RuleParser {
-    parseRule(rule: RuleArg): Rule | undefined
+  parseRule(rule: RuleArg): Rule | undefined
 }
 
 export {}
