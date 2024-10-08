08de5519bf31ac2d9d04b1e369e85857a95008c8
Anfo <longjiahui@hotmail.com>
2024-10-08 11:17:31 +0800

    feat: v3.0.1 优化ObjectRules类型，可以嵌套

--- src/typings/index.ts
+++ src/typings/index.ts
@@ -1,49 +1,58 @@
-import type { Validator } from ".."
-import type { default as presetRules } from "../presetRules"
+import type { Validator } from '..'
+import type { default as presetRules } from '../presetRules'
 
 export type Rule<D = any> = (
-  val: D,
-  v: Validator,
-  errorDescription?: string
+    val: D,
+    v: Validator,
+    errorDescription?: string
 ) => PromiseLike<string | void | undefined> | string | void | undefined
 export type RuleReturn = ReturnType<Rule>
 export type AwaitedRuleReturn = Awaited<RuleReturn>
 export interface Module {
-  getParser(v: Validator): RuleParser
+    getParser(v: Validator): RuleParser
 }
 
 export type PresetRules = keyof typeof presetRules
 
 export interface RuleExtend {
-  base: Rule
-  string: string
-  boolean: boolean
-  regexp: RegExp
-  number: number
+    base: Rule
+    string: string
+    boolean: boolean
+    regexp: RegExp
+    number: number
 }
 
 export type GetRuleArg<T> = T[keyof T]
 export type RuleArg =
-  | PresetRules
-  | GetRuleArg<RuleExtend>
-  | {
-      /** for object recursive*/
-      [k: string]: RuleArg | undefined
-      $or?: RuleArg[]
-      $subItem?: RuleArg
-    }
-  | /*for and */ RuleArg[]
-  | {
-      $or?: RuleArg[]
-      $subItem?: RuleArg
-    }
+    | PresetRules
+    | GetRuleArg<RuleExtend>
+    | {
+          /** for object recursive*/
+          [k: string]: RuleArg | undefined
+          $or?: RuleArg[]
+          $subItem?: RuleArg
+      }
+    | /*for and */ RuleArg[]
+    | {
+          $or?: RuleArg[]
+          $subItem?: RuleArg
+      }
 
-export type ObjectRules<T extends object> = Partial<
-  Record<keyof T extends string ? `${keyof T}$` | keyof T : never, RuleArg>
->
+// export type ObjectRules<T extends object> = Partial<
+//   Record<keyof T extends string ? `${keyof T}$` | keyof T : never, RuleArg>
+// >
+export type ObjectRules<T extends object> = Partial<{
+    [k in keyof T extends string
+        ? `${keyof T}$` | keyof T
+        : never]: k extends keyof T
+        ? T[k] extends object
+            ? ObjectRules<T[k]> | RuleArg
+            : RuleArg
+        : RuleArg
+}>
 
 export interface RuleParser {
-  parseRule(rule: RuleArg): Rule | undefined
+    parseRule(rule: RuleArg): Rule | undefined
 }
 
 export {}
